0622fcef9d3605683b1b43ce8956b2087da2a076
Ryan Carpenter <rccarpenter@outlook.com>
2021-01-01 12:25:23 -0700

    init

--- client/src/AuthConfig.js
+++ client/src/AuthConfig.js
@@ -1,3 +1,3 @@
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'emberglo.us.auth0.com'
+export const clientId = 'GO3c8qwu00d4kyOojt1sPKVlWAyl41Wi'
+export const audience = 'http://www.keepr.com'
